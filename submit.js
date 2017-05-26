adac3a7c4e904cd0a0cfd6088688a167b64d466e
William Goodall <wgoodall01@gmail.com>
2017-05-26 14:20:30 -0400

    More changes

--- submit.js
+++ submit.js
@@ -2,6 +2,8 @@ const url = "https://hooks.slack.com/services/T5KET0TM4/B5JPFALJ1/xEGAzD7p5dxgYM
 
 document.addEventListener("DOMContentLoaded", ()=>{
 	const button = document.getElementById("submit-button")
+	const submissionPage = document.getElementById("submission");
+	const resultPage = document.getElementById("result");
 	const getRoom = () => document.getElementById("room-input").value;
 	const getProblem = () => document.getElementById("problem-input").value;
 
@@ -15,5 +17,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", `${url}?payload=${encodeURIComponent(JSON.stringify(body))}`);
 		xhr.send(JSON.stringify(body));
+
+		resultPage.classList.remove("hidden");
+		submissionPage.classList.add("hidden");
 	})
 })
