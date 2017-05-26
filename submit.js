6c4eb20391dbe75ad16a4145cfa607771fe1bee1
William Goodall <wgoodall01@gmail.com>
2017-05-26 14:15:53 -0400

    Initial commit

---
+++ submit.js
@@ -0,0 +1,19 @@
+const url = "https://hooks.slack.com/services/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";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	const button = document.getElementById("submit-button")
+	const getRoom = () => document.getElementById("room-input").value;
+	const getProblem = () => document.getElementById("problem-input").value;
+
+	button.addEventListener("click", ()=>{
+		const body = {
+			mrkdwn:false, // stop injection attacks
+			text:`Someone in room ${getRoom()} needs help with ${getProblem()}!`,
+			username:"MKA Help Button",
+		}
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", `${url}?payload=${encodeURIComponent(JSON.stringify(body))}`);
+		xhr.send(JSON.stringify(body));
+	})
+})
